edb0432030ba263434589956e61c35e28997d0c6
Lucas Carlson <lucas@carlson.net>
2021-03-01 12:52:57 -0800

    Adding CLOUDENV_BEARER_TOKEN, CLOUDENV_APP_SLUG and CLOUDENV_APP_SECRET_KEY checks as options to help streamline CI/CD pipelines

--- cloudenv-hq.gemspec
+++ cloudenv-hq.gemspec
@@ -1,18 +1,18 @@
 Gem::Specification.new do |s|
-  s.name = "cloudenv-hq"
-  s.version = "0.2.0"
-  s.license = "MIT"
-  s.version = "#{s.version}-alpha-#{ENV['TRAVIS_BUILD_NUMBER']}" if ENV["TRAVIS"]
-  s.date = "2020-10-10"
-  s.summary = "Keep your ENV vars synced between your team members"
-  s.email = "support@cloudenv.com"
-  s.homepage = "https://github.com/cloudenvhq/cloudenv-ruby"
-  s.description = "Keep your ENV vars synced between your team members."
-  s.authors = ["Lucas Carlson"]
-  s.files = ["install.rb", "lib", "lib/cloudenv-hq.rb", "LICENSE", "Rakefile", "README.markdown", "cloudenv-hq.gemspec", "test", "test/base", "test/base/base_test.rb", "test/data", "test/test_helper.rb"]
-  s.rubyforge_project = "cloudenv-hq"
-  s.add_dependency "dotenv"
-  s.add_development_dependency "rake"
-  s.add_development_dependency "rdoc"
-  s.add_development_dependency "test-unit"
+  s.name = 'cloudenv-hq'
+  s.version = '0.2.5'
+  s.license = 'MIT'
+  s.date = '2020-10-10'
+  s.summary = 'Keep your ENV vars synced between your team members'
+  s.email = 'support@cloudenv.com'
+  s.homepage = 'https://github.com/cloudenvhq/cloudenv-ruby'
+  s.description = 'Keep your ENV vars synced between your team members.'
+  s.authors = ['Lucas Carlson']
+  s.files = ['install.rb', 'lib', 'lib/cloudenv-hq.rb', 'LICENSE', 'Rakefile', 'README.markdown', 'cloudenv-hq.gemspec',
+             'test', 'test/base', 'test/base/base_test.rb', 'test/data', 'test/test_helper.rb']
+  s.rubyforge_project = 'cloudenv-hq'
+  s.add_dependency 'dotenv'
+  s.add_development_dependency 'rake'
+  s.add_development_dependency 'rdoc'
+  s.add_development_dependency 'test-unit'
 end
--- lib/cloudenv-hq.rb
+++ lib/cloudenv-hq.rb
@@ -1,33 +1,38 @@
-require "pathname"
-require "dotenv"
-require "tempfile"
-require "yaml"
+require 'pathname'
+require 'dotenv'
+require 'tempfile'
+require 'yaml'
 
 class CloudenvHQ
-  VERSION = "0.2.0".freeze
+  VERSION = '0.2.5'.freeze
 
-  API_HOST = "https://app.cloudenv.com".freeze
-  READ_PATH = "/api/v1/envs".freeze
+  API_HOST = 'https://app.cloudenv.com'.freeze
+  READ_PATH = '/api/v1/envs'.freeze
 
-  def initialize(options={})
-    @environment = ENV["RAILS_ENV"] || ENV["RACK_ENV"]
-    @bearer = ENV["CLOUDENV_BEARER_TOKEN"] || `cat #{options[:bearer] || "~/.cloudenvrc"}`.strip
-    @secret_key_filename = ".cloudenv-secret-key"
+  def initialize(options = {})
+    @environment = ENV['RAILS_ENV'] || ENV['RACK_ENV']
+    @bearer = ENV['CLOUDENV_BEARER_TOKEN'] || `cat #{options[:bearer] || '~/.cloudenvrc'}`.strip
+    @secret_key_filename = '.cloudenv-secret-key'
     @secret_key = Pathname.new(@secret_key_filename)
 
-    until File.exists?(@secret_key) || @secret_key.expand_path == Pathname.new("/.cloudenv-secret-key")
+    until File.exist?(@secret_key) || @secret_key.expand_path == Pathname.new('/.cloudenv-secret-key')
       @secret_key = @secret_key.parent.parent + @secret_key_filename
     end
 
-    if File.exists?(@secret_key)
-      data = YAML.load(IO.read(@secret_key))
+    if File.exist?(@secret_key)
+      data = YAML.safe_load(IO.read(@secret_key))
 
-      @app = data["slug"]
-      @secret_key = data["secret-key"]
+      @app = data['slug']
+      @secret_key = data['secret-key']
+    else
+      @app = ENV['CLOUDENV_APP_SLUG']
+      @secret_key = ENV['CLOUDENV_APP_SECRET_KEY']
+    end
 
+    if @app && @secret_key
       if @environment
         data = `curl -s -H "Authorization: Bearer #{@bearer}" "https://app.cloudenv.com/api/v1/envs?name=#{@app}&environment=#{@environment}&version=#{VERSION}&lang=ruby" | openssl enc -a -aes-256-cbc -md sha512 -d -pass pass:"#{@secret_key}" 2> /dev/null`
-        file = Tempfile.new("cloudenv")
+        file = Tempfile.new('cloudenv')
         file.write(data)
         file.close
         Dotenv.load(file.path)
@@ -35,15 +40,15 @@ class CloudenvHQ
       end
 
       data = `curl -s -H "Authorization: Bearer #{@bearer}" "https://app.cloudenv.com/api/v1/envs?name=#{@app}&environment=default&version=#{VERSION}&lang=ruby" | openssl enc -a -aes-256-cbc -md sha512 -d -pass pass:"#{@secret_key}" 2> /dev/null`
-      file = Tempfile.new("cloudenv")
+      file = Tempfile.new('cloudenv')
       file.write(data)
       file.close
       Dotenv.load(file.path)
       file.unlink
     else
-      warn "WARNING: cloudenv could not find a .cloudenv-secret-key in the directory path"
+      warn 'WARNING: cloudenv could not find a .cloudenv-secret-key in the directory path or values for both ENV["CLOUDENV_APP_SLUG"] and ENV["CLOUDENV_APP_SECRET_KEY"]'
     end
   end
 end
 
-CloudenvHQ.new(bearer: ENV["CLOUDENV_BEARER_PATH"])
+CloudenvHQ.new(bearer: ENV['CLOUDENV_BEARER_PATH'])
