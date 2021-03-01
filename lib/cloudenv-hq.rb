6d15a72848ebb8784a14911ba1e966feac72d364
Lucas Carlson <lucas@carlson.net>
2021-03-01 13:36:39 -0800

    additional checks for environmental variables

--- lib/cloudenv-hq.rb
+++ lib/cloudenv-hq.rb
@@ -4,34 +4,43 @@ require 'tempfile'
 require 'yaml'
 
 class CloudenvHQ
-  VERSION = '0.2.5'.freeze
+  VERSION = '0.2.7'.freeze
 
   API_HOST = 'https://app.cloudenv.com'.freeze
   READ_PATH = '/api/v1/envs'.freeze
 
   def initialize(options = {})
     @environment = ENV['RAILS_ENV'] || ENV['RACK_ENV']
-    @bearer = ENV['CLOUDENV_BEARER_TOKEN'] || `cat #{options[:bearer] || '~/.cloudenvrc'}`.strip
-    @secret_key_filename = '.cloudenv-secret-key'
-    @secret_key = Pathname.new(@secret_key_filename)
 
-    until File.exist?(@secret_key) || @secret_key.expand_path == Pathname.new('/.cloudenv-secret-key')
-      @secret_key = @secret_key.parent.parent + @secret_key_filename
+    if ENV['CLOUDENV_BEARER_TOKEN'] || options[:bearer]
+      @bearer = ENV['CLOUDENV_BEARER_TOKEN'] || options[:bearer]
+    else
+      @bearer_filename = File.expand_path(ENV['CLOUDENV_BEARER_FILE'] || '~/.cloudenvrc')
+      @bearer = IO.read(@bearer_filename).to_s.strip if File.exist?(@bearer_filename)
     end
 
-    if File.exist?(@secret_key)
-      data = YAML.safe_load(IO.read(@secret_key))
-
-      @app = data['slug']
-      @secret_key = data['secret-key']
-    else
+    if ENV['CLOUDENV_APP_SLUG'] && ENV['CLOUDENV_APP_SECRET_KEY']
       @app = ENV['CLOUDENV_APP_SLUG']
       @secret_key = ENV['CLOUDENV_APP_SECRET_KEY']
+    else
+      @secret_key_filename = File.expand_path(ENV['CLOUDENV_APP_SECRET_KEY_FILE'] || '.cloudenv-secret-key')
+      @secret_key = Pathname.new(@secret_key_filename)
+
+      until File.exist?(@secret_key) || @secret_key.expand_path == Pathname.new('/.cloudenv-secret-key')
+        @secret_key = @secret_key.parent.parent + @secret_key_filename
+      end
+
+      if File.exist?(@secret_key)
+        data = YAML.safe_load(IO.read(@secret_key))
+
+        @app = data['slug']
+        @secret_key = data['secret-key']
+      end
     end
 
-    if @app && @secret_key
+    if @bearer && @app && @secret_key
       if @environment
-        data = `curl -s -H "Authorization: Bearer #{@bearer}" "https://app.cloudenv.com/api/v1/envs?name=#{@app}&environment=#{@environment}&version=#{VERSION}&lang=ruby" | openssl enc -a -aes-256-cbc -md sha512 -d -pass pass:"#{@secret_key}" 2> /dev/null`
+        data = `curl -s -H "Authorization: Bearer #{@bearer}" "#{API_HOST}#{READ_PATH}?name=#{@app}&environment=#{@environment}&version=#{VERSION}&lang=ruby" | openssl enc -a -aes-256-cbc -md sha512 -d -pass pass:"#{@secret_key}" 2> /dev/null`
         file = Tempfile.new('cloudenv')
         file.write(data)
         file.close
@@ -39,7 +48,7 @@ class CloudenvHQ
         file.unlink
       end
 
-      data = `curl -s -H "Authorization: Bearer #{@bearer}" "https://app.cloudenv.com/api/v1/envs?name=#{@app}&environment=default&version=#{VERSION}&lang=ruby" | openssl enc -a -aes-256-cbc -md sha512 -d -pass pass:"#{@secret_key}" 2> /dev/null`
+      data = `curl -s -H "Authorization: Bearer #{@bearer}" "#{API_HOST}#{READ_PATH}?name=#{@app}&environment=default&version=#{VERSION}&lang=ruby" | openssl enc -a -aes-256-cbc -md sha512 -d -pass pass:"#{@secret_key}" 2> /dev/null`
       file = Tempfile.new('cloudenv')
       file.write(data)
       file.close
